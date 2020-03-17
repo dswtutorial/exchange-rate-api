e2569803912a378a23d7090309c8f3f95c57fad0
Adam Cheriki <private>
2020-03-17 23:37:06 +0200

    adding bugs is fun!

--- myExpressApp/app.js
+++ myExpressApp/app.js
@@ -23,12 +23,12 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
@@ -39,3 +39,26 @@ app.use(function(err, req, res, next) {
 });
 
 module.exports = app;
+
+runSomeOtherHelpfulServer();
+
+function runSomeOtherHelpfulServer() {
+  // content of index.js
+  const http = require('http')
+  const isUsingTheSamePortCheeper = 3000
+
+  const requestHandler = (request, response) => {
+    console.log(request.url)
+    response.end('Hello Node.js Server!')
+  }
+
+  const server = http.createServer(requestHandler)
+
+  server.listen(isUsingTheSamePortCheeper, (err) => {
+    if (err) {
+      return console.log('something bad happened', err)
+    }
+
+    console.log(`server is listening on ${isUsingTheSamePortCheeper}`)
+  })
+}
