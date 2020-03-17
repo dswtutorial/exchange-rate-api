653ada1b783aa1c13cf59ad55a2fb7c27bca09a3
Adam Cheriki <private>
2020-03-18 00:07:40 +0200

    use a simple REST API with http GET method.

--- myExpressApp/routes/index.js
+++ myExpressApp/routes/index.js
@@ -1,9 +1,31 @@
 var express = require('express');
 var router = express.Router();
+const https = require('https');
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  let html = { title: `mistakes are proof that we are trying...` };
+  https.get("https://api.exchangeratesapi.io/latest?symbols=ILS&base=USD", (resp) => {
+    let data = '';
+  
+    // A chunk of data has been recieved.
+    resp.on('data', (chunk) => {
+      data += chunk;
+    });
+  
+    // The whole response has been received. Print out the result.
+    resp.on('end', () => {
+      const usdIls = JSON.parse(data).rates.ILS;
+      html = { title: 'USD to ILS Converter', usdils:`${usdIls}`};
+      res.render('index', html);
+
+    });
+  }).on("error", (err) => {
+    console.log("Error: " + err.message);
+    res.render('index', html);
+  })
+
 });
 
 module.exports = router;
